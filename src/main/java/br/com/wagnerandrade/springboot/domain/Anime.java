b823597e6d01993a58942e8d0faf97ace21e10c9
Wagner Andrade <wagnerpbandrade@gmail.com>
2020-12-06 14:51:17 -0300

    Add dependências jpa e mysql, configurando conexão e add anotação de entidade em Anime

--- src/main/java/br/com/wagnerandrade/springboot/domain/Anime.java
+++ src/main/java/br/com/wagnerandrade/springboot/domain/Anime.java
@@ -2,13 +2,23 @@ package br.com.wagnerandrade.springboot.domain;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
+@Entity
 public class Anime {
 
     private static final long serialVersionUID = -9177100189252636092L;
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
 
