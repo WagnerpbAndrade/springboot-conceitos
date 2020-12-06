83147e9eb78ba3e6a73521865de9192ef3e0e71d
Wagner Andrade <wagnerpbandrade@gmail.com>
2020-12-06 04:02:24 -0300

    Criando métodos Get, trocando application.properties para .yml e removendo stacktrace erro

--- src/main/java/br/com/wagnerandrade/springboot/controller/AnimeController.java
+++ src/main/java/br/com/wagnerandrade/springboot/controller/AnimeController.java
@@ -1,10 +1,15 @@
 package br.com.wagnerandrade.springboot.controller;
 
 import br.com.wagnerandrade.springboot.domain.Anime;
+import br.com.wagnerandrade.springboot.service.AnimeService;
 import br.com.wagnerandrade.springboot.util.DateUtil;
 import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,16 +17,22 @@ import java.time.LocalDateTime;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/v1/animes")
+@RequestMapping("api/v1/animes")
 @Log4j2
-@AllArgsConstructor
+@RequiredArgsConstructor
 public class AnimeController {
-    private DateUtil dateUtil;
+    private final DateUtil dateUtil;
+    private final AnimeService animeService;
 
     @GetMapping
-    public List<Anime> list() {
+    public ResponseEntity<List<Anime>> list() {
         log.info(dateUtil.formaLocalDateTimeToDatabaseStyle(LocalDateTime.now()));
-        return List.of(new Anime("DBZ"), new Anime("Berserk"));
+        return ResponseEntity.ok(animeService.listAll());
+    }
+
+    @GetMapping(path = "/{id}")
+    public ResponseEntity<Anime> findById(@PathVariable("id") Long id) {
+        return ResponseEntity.ok(animeService.findById(id));
     }
 
 }
--- src/main/java/br/com/wagnerandrade/springboot/domain/Anime.java
+++ src/main/java/br/com/wagnerandrade/springboot/domain/Anime.java
@@ -1,23 +1,15 @@
 package br.com.wagnerandrade.springboot.domain;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
 public class Anime {
 
     private static final long serialVersionUID = -9177100189252636092L;
 
+    private Long id;
     private String name;
 
-    public Anime() {
-    }
-
-    public Anime(String name) {
-        this.name = name;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
 }
