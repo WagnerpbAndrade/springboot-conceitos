53d51da9bcf9e8d7f9fe3d820e5baad853de746e
Wagner Andrade <wagnerpbandrade@gmail.com>
2020-12-07 02:56:38 -0300

    Sorting e Log SQL padrão spring

--- src/main/java/br/com/wagnerandrade/springboot/configurer/ConceitosWebMvcConfigurer.java
+++ src/main/java/br/com/wagnerandrade/springboot/configurer/ConceitosWebMvcConfigurer.java
@@ -9,7 +9,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import java.util.List;
 
 @Configuration
-public class ConceitosWebMvnConfigurer implements WebMvcConfigurer {
+public class ConceitosWebMvcConfigurer implements WebMvcConfigurer {
     @Override
     public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
         PageableHandlerMethodArgumentResolver pageHandler = new PageableHandlerMethodArgumentResolver();
