c636e0bfcfaad3b5e02713f9ce2c723e693ee2b1
Wagner Andrade <wagnerpbandrade@gmail.com>
2020-12-15 21:34:56 -0300

    Spring Data JPA Test - Exceções

--- src/test/java/br/com/wagnerandrade/springboot/repository/AnimeRepositoryTest.java
+++ src/test/java/br/com/wagnerandrade/springboot/repository/AnimeRepositoryTest.java
@@ -8,6 +8,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
+import javax.validation.ConstraintViolationException;
 import java.util.List;
 import java.util.Optional;
 
@@ -78,9 +79,9 @@ class AnimeRepositoryTest {
 
         List<Anime> animes = this.animeRepository.findByName(name);
 
-        Assertions.assertThat(animes).isNotEmpty();
-
-        Assertions.assertThat(animes).contains(animeSaved);
+        Assertions.assertThat(animes)
+                .isNotEmpty()
+                .contains(animeSaved);
     }
 
     @Test
@@ -89,6 +90,20 @@ class AnimeRepositoryTest {
         List<Anime> animes = this.animeRepository.findByName("Wagner");
 
         Assertions.assertThat(animes).isEmpty();
+    }
+
+    @Test
+    @DisplayName("Save thow ConstraintViolationException when name is empty")
+    void save_ThrowConstraintViolationException_WhenNameIsEmpty() {
+        Anime animeToBeSaved = new Anime();
+
+//        Assertions.assertThatThrownBy(() -> this.animeRepository.save(animeToBeSaved))
+//                .isInstanceOf(ConstraintViolationException.class);
+
+        Assertions.assertThatExceptionOfType(ConstraintViolationException.class)
+                .isThrownBy(() -> this.animeRepository.save(animeToBeSaved))
+                .withMessageContaining("The anime name cannot be empty");
+
     }
 
     private Anime createAnime() {
