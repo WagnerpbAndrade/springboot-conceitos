c1c75816d9553a520390bad4c57df94542283476
Wagner Andrade <wagnerpbandrade@gmail.com>
2020-12-16 21:20:14 -0300

    CSRF Token - Spring Security

--- src/main/java/br/com/wagnerandrade/springboot/config/SecurityConfig.java
+++ src/main/java/br/com/wagnerandrade/springboot/config/SecurityConfig.java
@@ -6,6 +6,7 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 
 import java.util.logging.Logger;
 
@@ -16,7 +17,9 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests()
+        http.csrf().disable()
+                //.csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
+                .authorizeRequests()
                 .anyRequest()
                 .authenticated()
                 .and()
